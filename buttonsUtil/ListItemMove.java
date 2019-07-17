eb0d9e3713dc21c7e97efa01a0f1fd9a446393b6
k0bu <30616294+k0bu@users.noreply.github.com>
2019-07-17 16:10:22 +0900

    I do not like this change to ListItemMove...

--- buttonsUtil/ListItemMove.java
+++ buttonsUtil/ListItemMove.java
@@ -7,18 +7,23 @@ import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
 
+import bookInterface.*;
+
 //https://ateraimemo.com/Swing/FastRemoveOfListItems.html
 public class ListItemMove {
-  public static <E> void move(JList<E> from, JList<E> to) {
+  public static void move(JList<String> from, JList<String> to, BookShelf fromShelf, BookShelf toShelf) {
     ListSelectionModel sm = from.getSelectionModel();
     int[] selectedIndices = from.getSelectedIndices();
   
-    DefaultListModel<E> fromModel = (DefaultListModel<E>) from.getModel();
-    DefaultListModel<E> toModel = (DefaultListModel<E>) to.getModel();
-    List<E> unselectedValues = new ArrayList<>();
+    DefaultListModel<String> fromModel = (DefaultListModel<String>) from.getModel();
+    DefaultListModel<String> toModel = (DefaultListModel<String>) to.getModel();
+    List<String> selectedValues = new ArrayList<>();
+    List<String> unselectedValues = new ArrayList<>();
     for (int i = 0; i < fromModel.getSize(); i++) {
       if (!sm.isSelectedIndex(i)) {
         unselectedValues.add(fromModel.getElementAt(i));
+      } else {
+        selectedValues.add(fromModel.getElementAt(i));
       }
     }
     if (selectedIndices.length > 0) {
@@ -26,8 +31,24 @@ public class ListItemMove {
         toModel.addElement(fromModel.get(i));
       }
       fromModel.clear();
+
+      selectedValues.forEach(s->{
+        Book b = new Book(s);
+        fromShelf.removeElement(b);
+      });
+
+      selectedValues.forEach(s->{
+        Book b = new Book(s);
+        toShelf.addElement(b);
+      });
+
+      System.out.println("FROMLIST: ");
+      System.out.println(fromShelf.toString());
+      System.out.println("TOLIST: ");
+      System.out.println(toShelf.toString());
+
       // unselectedValues.forEach(fromModel::addElement);
-      DefaultListModel<E> model = new DefaultListModel<>();
+      DefaultListModel<String> model = new DefaultListModel<>();
       unselectedValues.forEach(model::addElement);
       from.setModel(model);
     }
