2bfc4bc8c9521a79ab14b9338448e5d626c95861
k0bu <30616294+k0bu@users.noreply.github.com>
2019-07-17 12:41:08 +0900

    Just a comment that explains making e isn't good

--- bookInterface/Aggregation.java
+++ bookInterface/Aggregation.java
@@ -2,6 +2,9 @@ package bookInterface;
 import java.util.List;
 
 public interface Aggregation{
+  //Generating instance in aggregation makes the connection too strong.
+  //In that case, When changees is needed in constructor, 
+  //there is necessary for changes in Aggreagation as well.
   public void addElement(Element e);
   public void setAggregation(List<Element> elements);
   public void removeElement(Element e);
