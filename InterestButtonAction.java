eb4c7fa39f54f18eb1033ecc4db5719823d2c5c0
k0bu <kazuki.tada@keio.jp>
2019-07-26 11:01:33 +0900

    modified file relevant to Recommend

--- InterestButtonAction.java
+++ InterestButtonAction.java
@@ -1,8 +1,11 @@
-package book_manager.buttonsUtil;
+
+//package book_manager.buttonsUtil;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.DefaultListModel;
@@ -10,18 +13,24 @@ import javax.swing.JList;
 import javax.swing.JTextField;
 
 import book_manager.bookInterface.*;
+import book_manager.buttonsUtil.*;
 
-public class PopularityButtonAction implements ActionListener{
+public class InterestButtonAction implements ActionListener{
   private BookShelf _toReadBookShelf;
   private JList<String> _toReadList;
 
-  public PopularityButtonAction(BookShelf toReadBookShelf, JList<String> toReadList){
+  public InterestButtonAction(BookShelf toReadBookShelf, JList<String> toReadList){
     this._toReadBookShelf = toReadBookShelf;
     this._toReadList = toReadList;
   }
 
   public void actionPerformed(ActionEvent e){
-    this._toReadBookShelf.setAggregation(Recommend.popularity(this._toReadBookShelf.getBooks()));
+    var list = Recommend.interest(this._toReadBookShelf.getBooks());
+    List<Element> item = new ArrayList<>();
+    ((Iterable<Book>) list).forEach(i -> {
+      item.add((Element)i);
+    });
+    this._toReadBookShelf.setAggregation(item);
     this._toReadList.setModel(new ListModelUtil<String>().List2ListModel(this._toReadBookShelf.toStrings()));
   }
 }
--- PopularityButtonAction.java
+++ PopularityButtonAction.java
@@ -1,27 +1,36 @@
-package book_manager.buttonsUtil;
+
+//package book_manager.buttonsUtil;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JTextField;
+import java.util.List;
 
 import book_manager.bookInterface.*;
+import book_manager.buttonsUtil.*;
 
-public class InterestButtonAction implements ActionListener{
+public class PopularityButtonAction implements ActionListener{
   private BookShelf _toReadBookShelf;
   private JList<String> _toReadList;
 
-  public InterestButtonAction(BookShelf toReadBookShelf, JList<String> toReadList){
+  public PopularityButtonAction(BookShelf toReadBookShelf, JList<String> toReadList){
     this._toReadBookShelf = toReadBookShelf;
     this._toReadList = toReadList;
   }
 
   public void actionPerformed(ActionEvent e){
-    this._toReadBookShelf.setAggregation(Recommend.interest(this._toReadBookShelf.getBooks()));
+    var list = Recommend.popularity(this._toReadBookShelf.getBooks());
+    List<Element> item = new ArrayList<>();
+    ((Iterable<Book>) list).forEach(i -> {
+      item.add((Element)i);
+    });
+    this._toReadBookShelf.setAggregation(item);
     this._toReadList.setModel(new ListModelUtil<String>().List2ListModel(this._toReadBookShelf.toStrings()));
   }
 }
